8f1b404e8e324f92c17a0e42c356c781078267b1
garikisraelyan <60008825+garikisraelyan@users.noreply.github.com>
2020-03-11 20:40:01 +0400

    Create name_valid.py

---
+++ name_valid.py
@@ -0,0 +1,19 @@
+user_name = input("Write some name: ")
+
+def check(x):
+	if x == x.title():
+		names_list = x.split()
+		if len(names_list) == 3:
+			if (names_list[0][1] == "." and len(names_list[0]) == 2 or names_list[1][1] == "." and len(names_list[1]) == 2 or (len(names_list[0]) > 1 and "." not in names_list[0]) or (len(names_list[1]) > 1 and "." not in names_list[1])) and len(names_list[2]) > 1 and "." not in names_list[2]:
+				return True
+			else:
+				return False
+		elif len(names_list) == 2:
+			if names_list[0][1] == "." and len(names_list[0]) == 2 or names_list[1][1] == "." and len(names_list[1]) == 2 or (len(names_list[0]) > 1 and "." not in names_list[0]) or (len(names_list[1]) > 1 and "." not in names_list[1]):
+				return True
+		else:
+			return False
+	else:
+		return False
+
+print(check(user_name))
