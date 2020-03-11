2ba62cfeef12f59d68880499ba533ff6ed8a9243
garikisraelyan <60008825+garikisraelyan@users.noreply.github.com>
2020-03-11 20:54:50 +0400

    Update name_valid.py

--- name_valid.py
+++ name_valid.py
@@ -4,7 +4,7 @@ def check(x):
 	if x == x.title():
 		names_list = x.split()
 		if len(names_list) == 3:
-			if (names_list[0][1] == "." and len(names_list[0]) == 2 or names_list[1][1] == "." and len(names_list[1]) == 2 or (len(names_list[0]) > 1 and "." not in names_list[0]) or (len(names_list[1]) > 1 and "." not in names_list[1])) and len(names_list[2]) > 1 and "." not in names_list[2]:
+			if (names_list[0][1] == "." and len(names_list[0]) == 2 and names_list[1][1] == "." and len(names_list[1]) == 2 or (len(names_list[0]) > 1 and "." not in names_list[0]) or (len(names_list[1]) > 1 and "." not in names_list[1])) and len(names_list[2]) > 1 and "." not in names_list[2]:
 				return True
 			else:
 				return False
